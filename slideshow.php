7ca069879c9d7370ddac173eb8c39a609a108d88
Zadok Maina <11650zadokmbutu@gmail.com>
2025-04-26 13:27:53 +0300

    Initial commit for Zeyd Music Shop

--- slideshow.php
+++ slideshow.php
@@ -124,7 +124,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 4000); // Change image every 4 seconds
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
 </script>
 
